526b90cc8c113916c4d64e42c890418d314d6adb
spiceyourcode <franklineomari@gmail.com>
2025-09-19 20:37:29 +0300

    implement product grid and load more functionality
    - Add CSS module for styling product grid and container
    - Update component to display products in a grid layout
    - Implement load more button to fetch additional products
    - Remove unused imageLink state and improve error handling

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -1,33 +1,55 @@
 import { useEffect, useState } from "react";
-
+import styles from './LoadMoreData.module.css'
 function LoadMoreData() {
 
     const [loading, setLoading] = useState(false);
     const [products, setProducts] = useState([]);
     const [count, setCount] = useState(0);
-    const [imageLink, setImageLink] = useState('')
 
-    async function fetchProducts(params) {
+
+    async function fetchProducts() {
         try {
             const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`);
             const result = await response.json();
-            console.log(result)
 
-            setImageLink(result.products[0].images[0])
-        
-            
+            if (result && result.products && result.products.length) {
+                setProducts([...products, ...result.products]);
+                setLoading(false);
+            }
+
+
         } catch (error) {
             console.log(e);
+            setLoading(false);
 
         }
     }
     useEffect(() => {
         fetchProducts()
-    }, [imageLink])
+    }, []);
+
+    if (loading) {
+        return <div> Loading Data....</div>
+    }
     return (
         <>
-            <div className="container">
-                <img src={imageLink} alt="" />
+            <div className={styles.container}>
+                {
+                    products && products.length ?
+                        <div className={styles['product-container']}>
+                            {products.map(
+                                item => <div className={styles.product} key={item.id}>
+                                    <img src={item.thumbnail} alt={item.title} />
+                                    <p>{item.title}</p>
+                                </div>
+                            )}
+                        </div>
+                        : null
+                }
+                <button onClick={() => {
+                    setCount(count + 1);
+                    fetchProducts();
+                }}>Load More</button>
             </div>
         </>
     );
