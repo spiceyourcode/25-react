844c4b594dd10c2d2ea60c8a4783f1cfaf53a574
spiceyourcode <franklineomari@gmail.com>
2025-09-24 22:51:27 +0300

    creating the load more data component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import Accordian from "./components/accordinan";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/start-rating";
 import ImageSlider from "./components/image-slider";
-
+import LoadMoreData from "./components/load-more-data"
+import TreeView from "./components/tree-view";
+import sidebarData from "./components/tree-view/data";
 function App() {
   return (
     <>
-      {/* <Accordian/>
-     <RandomColor/>
-     <StarRating noOfStars={10}/> */}
+      {/* 
+      
+      <Accordian />
+      <RandomColor />
+      <StarRating noOfStars={10} />
       <ImageSlider
         url={"https://picsum.photos/v2/list?"}
         page={"1"}
         limit={"10"}
-      />
+      />  
+      <LoadMoreData/>
+      */}
+      <TreeView menus={sidebarData} />
     </>
   );
 }
