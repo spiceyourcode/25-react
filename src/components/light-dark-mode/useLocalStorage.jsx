ab19c11e2ae75664a4175a39ef70a94f06b3db0e
spiceyourcode <franklineomari@gmail.com>
2025-11-09 23:14:06 +0300

    light dark mode v2 no testing done

---
+++ src/components/light-dark-mode/useLocalStorage.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function useLocalStorage(key, value){
+    const[value, setValue] = useState(()=>{
+        let currentValue;
+
+        try{
+            currentValue = JSON.parse(localStorage.getItem(key) || string(defaultValue));
+        } 
+        catch(error){
+            console.log(error);
+            currentValue = defaultValue
+        }
+        return currentValue;
+
+    });
+    useEffect(()=>{
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+    return [value, setValue];
+}
+
+export default useLocalStorage;
