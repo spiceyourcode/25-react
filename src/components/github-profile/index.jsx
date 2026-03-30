b0f727b8458ef432afb3c5b4c12fbdb78718e46a
spiceyourcode <franklineomari@gmail.com>
2026-03-30 11:42:19 +0300

    Finalizing on the github profile fetcher and adding the styles

--- src/components/github-profile/index.jsx
+++ src/components/github-profile/index.jsx
@@ -1,22 +1,40 @@
 import { useState } from "react";
+import "./profile.css";
 
 export default function GitHubProfileFinder() {
-    const [userName, setUserName] = useState("spiceyourcode")
-    function handleSubmit() {
+    const [userName, setUserName] = useState("spiceyourcode");
+    const [profile, setProfile] = useState(null);
+    async function handleSubmit() {
+        const response = await fetch(`https://api.github.com/users/${userName}`)
+        const data = await response.json()
+        setProfile(data)
     }
-
     return (
-        <div className="github-profile-container">
-            <div className="input-wrapper">
-                <input
-                    type="text"
-                    placeholder="Search Github Username.."
-                    value={userName}
-                    onChange={(e) => setUserName(e.target.value)}
-                />
+        <div className="profile-container">
+            <div className="search-container">
+                <div className="input-wrapper">
+                    <input
+                        type="text"
+                        placeholder="Search Github Username.."
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                </div>
+                <button onClick={handleSubmit}>Search</button>
             </div>
-            <button onClick={handleSubmit}>Search</button>
-        </div>
+
+            {
+                profile ? (
+                    <div className="user-container">
+                        <img className="avatar" src={profile.avatar_url} />
+                        <div className="name">{profile.name}</div>
+                        <div className="bio">{profile.bio}</div>
+                    </div>
+                ) : null
+            }
+
+
+        </div >
     )
 }
 
